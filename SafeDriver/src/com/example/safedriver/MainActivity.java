382f89563eb153f51349458207773e337a9bd903
Mitchell Vogel <mjv58@cornell.edu>
2013-10-05 13:33:07 -0400

    cleared compile time errors

--- SafeDriver/src/com/example/safedriver/MainActivity.java
+++ SafeDriver/src/com/example/safedriver/MainActivity.java
@@ -1,22 +1,77 @@
 package com.example.safedriver;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity  implements SensorEventListener{
+
+	public static float swRoll;
+	public static float swPitch;
+	public static float swAzimuth;
+
+
+	public static SensorManager mSensorManager;
+	public static Sensor accelerometer;
+	public static Sensor magnetometer;
+
+	public static float[] mAccelerometer = null;
+	public static float[] mGeomagnetic = null;
+
+
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+	}
 
 	@Override
+	public void onSensorChanged(SensorEvent event) {
+		// onSensorChanged gets called for each sensor so we have to remember the values
+		if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+			mAccelerometer = event.values;
+		}
+
+		if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD) {
+			mGeomagnetic = event.values;
+		}
+
+		if (mAccelerometer != null && mGeomagnetic != null) {
+			float R[] = new float[9];
+			float I[] = new float[9];
+			boolean success = SensorManager.getRotationMatrix(R, I, mAccelerometer, mGeomagnetic);
+
+			if (success) {
+				float orientation[] = new float[3];
+				SensorManager.getOrientation(R, orientation);
+				// at this point, orientation contains the azimuth(direction), pitch and roll values.
+				double azimuth = 180 * orientation[0] / Math.PI;
+				double pitch = 180 * orientation[1] / Math.PI;
+				double roll = 180 * orientation[2] / Math.PI;
+			}
+		}
+	}
+
+
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_main);
+		mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+		accelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+		magnetometer = mSensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+	}
+
+	protected void onResume() {
+		super.onResume();
+
+		mSensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+		mSensorManager.registerListener(this, magnetometer, SensorManager.SENSOR_DELAY_GAME);
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	protected void onPause() {
+		super.onPause();
+		mSensorManager.unregisterListener(this, accelerometer);
+		mSensorManager.unregisterListener(this, magnetometer);
 	}
-
 }
