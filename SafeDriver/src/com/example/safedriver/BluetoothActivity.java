abf5bef363df32fd8724ad3d19c1a533acc48ca7
shunfu <shunhua.fu@gmail.com>
2013-10-05 23:03:52 -0500

    Almost have bluetooth set up

--- SafeDriver/src/com/example/safedriver/BluetoothActivity.java
+++ SafeDriver/src/com/example/safedriver/BluetoothActivity.java
@@ -1,67 +1,62 @@
 package com.example.safedriver;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-
-import android.app.ActionBar;
 import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.util.Log;
-import android.view.KeyEvent;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
-import android.view.Window;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
-import android.widget.Toast;
+
 
 public class BluetoothActivity extends Activity {
+	TBlue tBlue;
+	TextView messagesTv;
 
-	/** Bluetooth Variables **/
-	
-	// Local Bluetooth adapter
-    private BluetoothAdapter mBluetoothAdapter = null;
+	// Debugging
+	private static final String TAG = "BluetoothActivity";
+	private static final boolean D = true;
 
-	private static BluetoothSocket btSocket;
-	private static InputStream btInStream;
-	private static OutputStream btOutStream;
-
-	private static final boolean D = true;          // Debug
-
-	OutputStream tmpOut = null;
-	OutputStream mmOutStream = null;    
-	InputStream tmpIn = null;
-	InputStream mmInStream = null;
-	byte[] buffer = new byte[1024];;  // buffer store for the stream
-	int bytes; // bytes returned from read()
-	int numberofbytes = 0;
+	// Intent request code
+	private final static int REQUEST_ENABLE_BT = 1;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		
-		// Get local Bluetooth adapter
-        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-
-        // If Bluetooth is not supported
-        if (mBluetoothAdapter == null) {
-            Toast.makeText(this, "Bluetooth is not available", Toast.LENGTH_LONG).show();
-            finish();
-            return;
-        }
+		initGUI();
 	}
 
+	@Override
+	public void onResume() {
+		super.onResume();
+		// If BT is not on, request that it be enabled.
+		if (!BluetoothAdapter.getDefaultAdapter().isEnabled()) {
+			Intent enableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+			startActivityForResult(enableIntent, REQUEST_ENABLE_BT);
+		}
+		tBlue = new TBlue();
+		if (tBlue.streaming()) {
+			messagesTv.append("Connected succesfully! ");
+		} else {
+			messagesTv.append("Error: Failed to connect. ");
+		}
+		String s="";
+		while (tBlue.streaming() && (s.length()<10) ) {
+			s+=tBlue.read();
+		}
+		messagesTv.append("Read from Bluetooth: \n"+s);
+	}
+
+	@Override
+	public void onPause() {
+		super.onPause();
+		tBlue.close();
+	}
+
+	public void initGUI() {
+		LinearLayout container=new LinearLayout(this);
+		messagesTv = new TextView(this);
+		container.addView(messagesTv);
+		setContentView(container);
+	}
 }
